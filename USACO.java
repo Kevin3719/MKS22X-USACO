4ea27e2d5e90d21444efe8c5e5562ccd746281d7
Kevin Chen <kchen01@stuy.edu>
2019-03-06 22:01:43 -0500

    done with bronze

--- USACO.java
+++ USACO.java
@@ -1,38 +1,47 @@
 import java.util.*;
+import java.io.*;
 public class USACO {
-  public int[][] cowstopper(int[][] input, int row, int col, int level) {
+  public static int[][] cowstopper(int[][] input, int row, int col, int level) {
     int max = 0;
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j ++) {
-        if (input[row + i][col + i] > max) {
-          max = input[row + i][col + i];
+        if (input[row + i][col + j] > max) {
+          max = input[row + i][col + j];
         }
       }
     }
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j ++) {
-        if (max - input[row + i][col + i] <= level) {
-          input[row + i][col + i] = max - level;
+        if (max - input[row + i][col + j] <= level) {
+          input[row + i][col + j] = max - level;
         }
       }
     }
     return input;
   }
-  public static int bronze(String filename) {
+  public static int bronze(String filename) throws FileNotFoundException {
     int output = 0;
-    Scanner s1 = new Scanner(filename);
-    int length = (int) s1.nextDouble();
-    int width = (int)s1.nextDouble();
-    int water = (int)s1.nextDouble();
-    int cows = (int)s1.nextDouble();
+    Scanner s1 = new Scanner(new File(filename));
+    int length = s1.nextInt();
+    int width = s1.nextInt();
+    int water = s1.nextInt();
+    int cows = s1.nextInt();
     int[][] lake = new int[length][width];
     for (int i = 0; i < length; i ++) {
       for (int j = 0; j < width; j++) {
-        lake[i][j] = (int) s1.nextDouble();
+        lake[i][j] = s1.nextInt();
       }
     }
-
-
+    for (int i = 0; i < cows; i++) {
+      cowstopper(lake,s1.nextInt() - 1, s1.nextInt() - 1,s1.nextInt());
+    }
+  for (int i = 0; i < length; i ++) {
+    for (int j = 0; j < width; j++) {
+      if (lake[i][j] < water) {
+        output += water - lake[i][j];
+      }
+    }
+  }
     return output * 72 * 72;
   }
 
